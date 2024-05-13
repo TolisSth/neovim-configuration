48ad5ebb1b847298770f8202accadcb92546ab3f
TolisSth <tolishalis@gmail.com>
2024-05-13 16:13:57 +0300

    Added fuzzy finder (Telescope) and deps

--- init.lua
+++ init.lua
@@ -21,6 +21,13 @@ Plug('windwp/nvim-autopairs')
 -- Theme
 Plug 'ellisonleao/gruvbox.nvim'
 
+-- Syntax highlighting / dependecy for telescope
+Plug 'nvim-treesitter/nvim-treesitter'
+
+-- Fuzzy finder
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+
 vim.call('plug#end')
 
 -- FYI: You will need to download this first: rustup component add rust-src
@@ -34,10 +41,29 @@ require'lspconfig'.rust_analyzer.setup{
   }
 }
 
+-- That's telescope, telescope needs ripgrep (Arch: ripgrep & rg) and fd (Arch: fd)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('nvim-treesitter.configs').setup {
+                ensure_installed = { 'rust', 'python' },
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+                -- Automatically install missing parsers when entering buffer
+                auto_install = false,
+                highlight = {
+                    enable = true,
+                },
+            }
+
 require'lspconfig'.rust_analyzer.setup{}
 require("mason").setup()
 require("mason-lspconfig").setup()
 require'lspconfig'.pyright.setup{}
 require("nvim-autopairs").setup {}
+
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
